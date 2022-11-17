68ba5a6db803f53b19f6f29dd98e847ca730525d
Tony Eng <tony.eng@telus.com>
2022-11-16 19:57:25 -0800

    finished findKeyByValue.js commit

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 🫠 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+const findKeyByValue = function(arg, arg2) {
+  for (let genre in arg) {
+    if (bestTVShowsByGenre[genre] === arg2) {// tip was to do bracket notation on genre to get key to populate
+      return genre;
+    }
+  }
+};
+
+//  console.log("testing", Object.keys(bestTVShowsByGenre));
+//  console.log("testing2", bestTVShowsByGenre.key);
+//  findKeyByValue(bestTVShowsByGenre);
+//  console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
