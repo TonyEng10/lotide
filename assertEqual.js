51ff8ae12cb0db3a9e2f246a3edcfaa101e2d399
Tony Eng <tony.eng@telus.com>
2022-11-21 08:28:27 -0800

    commit headTest.js and assertEqualTest.js both working

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Hello", "Hello");
-assertEqual(3, 5);
+module.exports = assertEqual;
+
+
---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+
+const head = require('../head');
+
+// assertEqual(head([8,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+console.log(head([8,6,7]));
+console.log(head(["Hello", "Lighthouse", "Labs"]));
