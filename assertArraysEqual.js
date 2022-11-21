5891efe7cddb443ac2dfd36b303f8ee97ea153c2
Tony Eng <tony.eng@telus.com>
2022-11-21 09:19:33 -0800

    assertArraysEqual.js commit but something weird is happenign with it

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-function eqArrays(arr1, arr2) {
-  let strArr1 = arr1.length;
-  let strArr2 = arr2.length;
-  if (strArr1 !== strArr2) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+// const eqArrays = require('../eqArrays');
 
 const assertArraysEqual = function(arg) {
   if (!arg) {
@@ -25,3 +13,5 @@ console.log(assertArraysEqual(eqArrays([1, 3], [1, 2])));
 
 console.log(eqArrays([1, 3], [1, 2]));
 console.log(eqArrays([1, 2], [1, 2]));
+
+module.exports = assertArraysEqual;
--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,13 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 🫠 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
-function eqArrays(arr1, arr2) {
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else if (actual !== expected) {
+//     console.log(`🛑🛑🛑 🫠 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+const eqArrays = function(arr1, arr2) {
   let strArr1 = arr1.length;// tip by mentor to try to check for false first to eliminate options
   let strArr2 = arr2.length;// compare length is a quick way to rule out comparison
   if (strArr1 !== strArr2) {// if not false then moves on to for loop
