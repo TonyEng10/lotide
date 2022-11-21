03444f9ab007fd3462a4fe2b516f7b6d27ce7311
Tony Eng <tony.eng@telus.com>
2022-11-21 08:38:31 -0800

    commit tailTest.js working

--- tail.js
+++ tail.js
@@ -11,8 +11,4 @@ const tail = function(data) {
   return x;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-console.log(tail(words));
+module.exports = tail;
