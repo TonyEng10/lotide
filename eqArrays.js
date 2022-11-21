9b1c848f818cbb534b781cdbe50334a086b417f8
Tony Eng <tony.eng@telus.com>
2022-11-21 08:47:37 -0800

    commit eqArrayTest.js working

--- eqArrays.js
+++ eqArrays.js
@@ -20,11 +20,8 @@ function eqArrays(arr1, arr2) {
   return true;// other wise return true after the for loop runs through 
 }
 
+module.exports = eqArrays;
 
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
