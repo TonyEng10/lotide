81f2cd686f89e5db14884dd99a94ea7a377a3f79
Tony Eng <tony.eng@telus.com>
2022-11-21 12:30:31 -0800

    git commit package.son files and updated headtest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,26 @@
 
+const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 // assertEqual(head([8,6,7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
-console.log(head([8,6,7]));
-console.log(head(["Hello", "Lighthouse", "Labs"]));
+
+// console.log(head([8,6,7]));
+// console.log(head(["Hello", "Lighthouse", "Labs"]));
+const assert = require('chai').assert;
+
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 8 for [8, 6, 7]", () => {
+    assert.strictEqual(head([8, 6, 7]), 8);
+  });
+});
