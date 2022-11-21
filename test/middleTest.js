1b15e68b99b81d1461645bd01a0373c827e4db21
Tony Eng <tony.eng@telus.com>
2022-11-21 13:15:55 -0800

    commit refactored middleTest.js and tailTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,20 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
 
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2]));
-console.log(middle([1]));
+describe("#middle", () => {
 
-console.log(assertArraysEqual(eqArrays(middle(([1, 2, 3, 4, 5])), [3])));
+  it("returns 3 for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3])
+  });
+
+});
+
+// console.log(middle([1, 2, 3, 4, 5]));
+// console.log(middle([1, 2, 3, 4]));
+// console.log(middle([1, 2]));
+// console.log(middle([1]));
+
+// console.log(assertArraysEqual(eqArrays(middle(([1, 2, 3, 4, 5])), [3])));
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,11 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
+
+describe("#tail", () => {
+
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5])
+  });
+});
+// console.log(tail(words));
